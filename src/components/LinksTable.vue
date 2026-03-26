20cb5faedc132e256f3e33d943eb21b6c7bf8aa1
Shane <6071159+smashedr@users.noreply.github.com>
2026-03-26 14:17:34 -0700

    Add processData to LinksTable.vue

--- src/components/LinksTable.vue
+++ src/components/LinksTable.vue
@@ -1,43 +1,58 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { processLinkOptions, processLinkFilters } from '@/utils/links.ts'
 import { useResults } from '@/composables/useResults.ts'
 import FilterSelect from '@/components/FilterSelect.vue'
+// import { useOptions } from '@/composables/useOptions.ts'
 
 console.debug('%cLOADED: components/LinksTable.vue', 'color: Orange')
 
 const results = useResults()
+// const options = useOptions()
 
-const links = ref()
+const linksRef = ref()
+const filterRef = ref()
 
-// TODO: watch options and update on options changes...
+// // TODO: INOP: watch options and update on options changes...
+// watch(
+//   options,
+//   async (data) => {
+//     console.log('%c WATCH: options:', 'color: Yellow', data)
+//   },
+//   { deep: true },
+// )
 
 watch(
   results,
   async (data) => {
-    console.log('%c RESULTS WATCH:', 'color: Yellow', data)
-    // TODO: Consider setting processed as a ref to use in onChanged
-    const processed = await processLinkOptions(data)
-    console.debug('processed:', processed)
-    links.value = processed
+    console.log('%c WATCH: results:', 'color: Yellow', data)
+    await processData()
   },
   { deep: true },
 )
 
-// TODO: This needs to be a ref and applied in the watcher above
-async function onChange(filter: Filter) {
-  console.debug('LinksTable.vue - onChange:', filter)
-  console.log('links.value:', links.value)
+async function processData() {
+  console.debug('processData: filterRef.value:', filterRef.value)
 
+  // Process Options
   const processed = await processLinkOptions(results.value)
   console.debug('processed:', processed)
 
-  if (filter) {
-    links.value = await processLinkFilters(filter, processed)
-    console.log('links.value:', links.value)
+  // Process Set Filter
+  if (filterRef.value) {
+    linksRef.value = await processLinkFilters(filterRef.value, processed)
+    console.log('linksRef.value:', linksRef.value)
   } else {
-    links.value = processed
-    console.log('links.value:', links.value)
+    linksRef.value = processed
+    console.log('linksRef.value:', linksRef.value)
   }
+}
+
+async function onChange(filter: Filter) {
+  console.debug('LinksTable.vue - onChange:', filter)
+  filterRef.value = filter
+  console.debug('filterRef.value:', filterRef.value)
+  await processData()
 }
 </script>
 
@@ -48,11 +63,11 @@ async function onChange(filter: Filter) {
       <table id="links-table" class="table table-sm table-striped table-hover small w-100">
         <thead class="">
           <tr>
-            <th>Links - {{ links?.length || 0 }}</th>
+            <th>Links - {{ linksRef?.length || 0 }}</th>
           </tr>
         </thead>
         <tbody id="links-body">
-          <tr v-if="links?.length" v-for="link of links">
+          <tr v-if="linksRef?.length" v-for="link of linksRef">
             <td>{{ link.href }}</td>
           </tr>
           <tr v-else>
